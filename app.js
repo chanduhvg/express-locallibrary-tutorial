fee7ab3b7a72720db19e7fa527c341c3f6f42f8f
chanduhvg <chanduhvg@gmail.com>
2020-05-01 15:34:40 -0500

    commit

--- app.js
+++ app.js
@@ -5,17 +5,30 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const dotenv = require('dotenv');
 
+
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
+const catalogRouter = require('./routes/catalog');
+
+const compression = require('compression');
+const helmet = require('helmet');
 
 const app = express();
-dotenv.config({ path: '.env' });
+app.use(helmet());
+app.use(compression()); //Compress all routes
+app.use(express.static(path.join(__dirname, 'public')));
+dotenv.config({ path: '.env' })
+
+// Set up mongoose connection
 const mongoose = require('mongoose');
-const mongoDB = process.env.ATLAS_URI;
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+//const dev_db_url = process.env.ATLAS_URI;
+const dev_db_url = 'mongodb+srv://chandhuhvg:Bgc@2468@cluster0-4gszn.azure.mongodb.net/local_library?retryWrites=true&w=majority'
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB, { useNewUrlParser: true ,useUnifiedTopology: true});
+mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,11 +38,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(helmet());
+app.use(compression());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/catalog', catalogRouter);  // Add catalog routes to middleware chain.
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
